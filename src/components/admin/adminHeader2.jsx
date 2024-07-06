727b1a973b369f90ee218a698f297dad3b5f3812
Bhawa1018 <124970079+Bhawa1018@users.noreply.github.com>
2024-07-06 15:02:20 +0530

    Update adminHeader2.jsx

--- src/components/admin/adminHeader2.jsx
+++ src/components/admin/adminHeader2.jsx
@@ -7,8 +7,100 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Avatar from "@mui/material/Avatar";
 import logo from "../../assets/Group 20.png";
-import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import InputAdornment from '@mui/material/InputAdornment';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import SortIcon from '@mui/icons-material/Sort';
+import TextField from "@mui/material/TextField";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: "3px",
+  backgroundColor: theme.palette.common.white,
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.94),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  height: "45px",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "#696969",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    [theme.breakpoints.up("md")]: {
+      width: "50ch",
+    },
+    color: "#696969",
+  },
+  "&::placeholder": {
+    color: "#696969",
+  },
+}));
+
+const Selection = styled(FormControl)(({ theme }) => ({
+  backgroundColor: theme.palette.common.white,
+  borderRadius: "3px",
+  border: "1px solid #ced4da",
+  height: "45px",
+  display: "flex",
+  alignItems: "left",
+  padding: "4px",
+  paddingLeft: "8px",
+  "& .MuiSelect-select": {
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: "6px",
+  },
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.94),
+  },
+  "&.Mui-focused": {
+    backgroundColor: alpha(theme.palette.common.white, 0.94),
+  },
+  width: "200px",
+}));
+
+const VerticalDivider = styled("div")(({ theme }) => ({
+  height: "28px",
+  width: "1px",
+  backgroundColor: "#00818A",
+  margin: "0 10px 0 0",
+}));
 
 export function MenuAppBarWithoutProgressBar({ onDepartmentChange, onSearchChange, onMostUsedClick }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -74,18 +166,17 @@ export function MenuAppBarWithoutProgressBar({ onDepartmentChange, onSearchChang
               }}
             >
               <Box>
-                <TextField
-                  id="standard-basic"
-                  label="Search by id"
-                  variant="standard"
-                  value={value}
-                  onChange={handleSearchChange}
-                  sx={{
-                    marginRight: "16px",
-                    backgroundColor: "#f0f0f0",
-                    borderRadius: 2,
-                  }}
-                />
+                <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    placeholder="Search by id"
+                    inputProps={{ "aria-label": "search" }}
+                    value={value}
+                    onChange={handleSearchChange}
+                  />
+                </Search>
               </Box>
               <Box sx={{ minWidth: "180px" }}>
                 <Button
@@ -103,32 +194,44 @@ export function MenuAppBarWithoutProgressBar({ onDepartmentChange, onSearchChang
                 </Button>
               </Box>
               <Box>
-                <TextField
-                  id="department-select"
-                  select
-                  label="Department"
-                  value={departmentValue}
-                  onChange={handleDepartmentChange}
-                  variant="standard"
-                  sx={{
-                    backgroundColor: "#f0f0f0",
-                    borderRadius: 2,
-                    width: "180px",
-                  }}
-                >
-                  <MenuItem key="All" value="All">
-                    All
-                  </MenuItem>
-                  <MenuItem key="IT" value="IT">
-                    IT
-                  </MenuItem>
-                  <MenuItem key="HR" value="HR">
-                    HR
-                  </MenuItem>
-                  <MenuItem key="Management" value="Management">
-                    Management
-                  </MenuItem>
-                </TextField>
+                <Selection variant="standard">
+                  <Select
+                    labelId="department-select-label"
+                    id="department-select"
+                    value={departmentValue}
+                    onChange={handleDepartmentChange}
+                    displayEmpty
+                    disableUnderline
+                    label="Department"
+                    startAdornment={
+                      <>
+                        <InputAdornment position="start">
+                          <FilterAltIcon />
+                        </InputAdornment>
+                        <VerticalDivider />
+                      </>
+                    }
+                    sx={{
+                      "& .MuiSelect-select": {
+                        padding: "7px 14px", // Add padding to space the text and icon properly
+                        textAlign: "center", // Align text to the left
+                      },
+                    }}
+                  >
+                    <MenuItem key="All" value="All">
+                      All
+                    </MenuItem>
+                    <MenuItem key="IT" value="IT">
+                      IT
+                    </MenuItem>
+                    <MenuItem key="HR" value="HR">
+                      HR
+                    </MenuItem>
+                    <MenuItem key="Management" value="Management">
+                      Management
+                    </MenuItem>
+                  </Select>
+                </Selection>
               </Box>
             </Box>
             <IconButton
